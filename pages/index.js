a76b9584f27c1d501b80447105e13917bfd410d0
Ian Hooey <ian.hooey@gmail.com>
2021-07-21 13:41:03 -0500

    Section 4

--- pages/index.js
+++ pages/index.js
@@ -1,16 +1,13 @@
-import Link from "next/link";
+import {getFeaturedEvents} from "../dummy-data";
+import EventList from "../components/events/event-list";
+import EventsSearch from "../components/events/events-search";
+import {Fragment} from "react";
 
 function HomePage() {
+  const featuredEvents = getFeaturedEvents()
+
   return <div>
-    <h1>The Home Page</h1>
-    <ul>
-      <li>
-        <Link href='/portfolio'>Portfolio</Link>
-      </li>
-      <li>
-        <Link href='/clients'>Clients</Link>
-      </li>
-    </ul>
+    <EventList items={featuredEvents}/>
   </div>
 }
 
